5f7f3e4bb698f8644f9d534e16e03a5123864f93
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 21:48:40 +0530

    Added:Stored the 4 results in a dictionary

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -x
+declare -A res
 read -p "Enter first number:" num1
 read -p "Enter second number:" num2
 read -p "Enter third number:" num3
 result1=$(($num1+$num2*$num3))
+res[1]=$result1
 result2=$(($num1*$num2+$num3))
+res[2]=$result2
 result3=$(($num3+$num1/$num2))
+res[3]=$result3
 result4=$(($num1%$num2+$num3))
-echo "The first result is "$result1
-echo "The second result is "$result2
-echo "The third result is "$result3
-echo "The fourth result is "$result4
+res[4]=$result4
+echo "The 4 results are" ${res[@]}
