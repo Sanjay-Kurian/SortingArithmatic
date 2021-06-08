28e5ce899d3f36d790787f420ae28d4cb5924d9c
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 21:10:45 +0530

    Added:Initialized three numbers

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -1,1 +1,8 @@
-#Welcome to Arithmatic Computation and Sorting problem.
+#!/bin/bash -x
+read -p "Enter first number:" x
+read -p "Enter second number:" y
+read -p "Enter third number:" z
+echo "The first number is:" $x
+echo "The second number is:" $y
+echo "The third number is:" $z
+
