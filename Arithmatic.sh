5136cf2929144f81cea8822f790fa13f68073acb
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 21:24:05 +0530

    Added
    git commit -m Added

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -3,4 +3,6 @@ read -p "Enter first number:" num1
 read -p "Enter second number:" num2
 read -p "Enter third number:" num3
 result1=$(($num1+$num2*$num3))
+result2=$(($num1*$num2+$num3))
 echo "The first result is " $result1
+echo "The second result is " $result2
