f114ea0103b8d3804477d14c76a1e47dec75594e
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 21:34:50 +0530

    Added:Computed fourth result

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -5,6 +5,8 @@ read -p "Enter third number:" num3
 result1=$(($num1+$num2*$num3))
 result2=$(($num1*$num2+$num3))
 result3=$(($num3+$num1/$num2))
+result4=$(($num1%$num2+$num3))
 echo "The first result is "$result1
 echo "The second result is "$result2
 echo "The third result is "$result3
+echo "The fourth result is "$result4
